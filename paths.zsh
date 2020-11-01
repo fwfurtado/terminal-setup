92bf3bd6cf0e1b83f504918ae44b62810cf479b4
Fernando <fwfurtado@gmail.com>
2020-11-01 02:36:35 -0300

    remove unused apps on path

--- paths.zsh
+++ paths.zsh
@@ -1,9 +1,10 @@
-export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
-export PATH="/usr/local/opt/findutils/libexec/gnubin:$PATH"
-export PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
-export PATH="/usr/local/opt/gnu-indent/libexec/gnubin:$PATH"
-export PATH="/usr/local/opt/gnu-getopt/bin:$PATH"
-export PATH="/usr/local/opt/grep/libexec/gnubin:$PATH"
-export PATH="/usr/local/opt/helm@2/bin:$PATH" # While istio does not support helm 3
+# export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
+# export PATH="/usr/local/opt/findutils/libexec/gnubin:$PATH"
+# export PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
+# export PATH="/usr/local/opt/gnu-indent/libexec/gnubin:$PATH"
+# export PATH="/usr/local/opt/gnu-getopt/bin:$PATH"
+# export PATH="/usr/local/opt/grep/libexec/gnubin:$PATH"
+# export PATH="/usr/local/opt/helm@2/bin:$PATH" # While istio does not support helm 3
+ 
+# export PATH="/usr/local/opt/mysql-client@5.7/bin:$PATH"
 
-export PATH="/usr/local/opt/mysql-client@5.7/bin:$PATH"
