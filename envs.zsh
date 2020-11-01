e6a6f0b4ac166c8edc1e378518941c9c52a5c622
Fernando <fwfurtado@gmail.com>
2020-11-01 02:34:58 -0300

    add asdf_home env var

--- envs.zsh
+++ envs.zsh
@@ -8,3 +8,6 @@ export FZF_DEFAULT_OPTS='--height 40% --layout=reverse'
 
 export ZSHSIFY_DIR="$HOME/.zinit/plugins/zdharma---zsh-startify/"
 export ERL_AFLAGS="-kernel shell_history enabled"
+
+export ASDF_HOME=/opt/asdf-vm
+export JAVA_HOME=/home/fwfurtado/.asdf/installs/java/adoptopenjdk-15.0.1+9
